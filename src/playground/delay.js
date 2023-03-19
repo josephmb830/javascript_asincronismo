9ec650d058545a362f0ab598040842856b683ac6
josephadolfomb <josephmobu@gmail.com>
2023-03-19 10:30:31 -0500

    Playground: Crea una función de delay que soporte asincronismo 12/26

---
+++ src/playground/delay.js
@@ -0,0 +1,10 @@
+export function delay(time, message) {
+        return new Promise((resolve, reject) => {
+            window.setTimeout(() => {
+            resolve (message);
+            }, time);
+        })
+    };
+
+    delay(2000, "Hello after 2s")
+    .then((message) => console.log(message))
